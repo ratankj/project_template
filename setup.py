1d1744ec8682ae3677f723f6d30426522615f26a
ratan kumar jha <imratankj@gamil.com>
2023-03-11 20:20:24 +0530

    pushing setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+PROJECT_NAME = "Machine Learning project"
+VERSION = "0.0.1"
+AUTHOR = "Ratan"
+DESCRIPTION = "This is our Machine learning project"
+
+REQUIREMENT_FILE_NAME = "requirements.txt"
+
+HYPHEN_E_DOT = "-e ."
+
+
+def get_requirements_list() -> List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+        requirement_list = [requirement_name.replace("\n", "") for requirement_name in requirement_list]
+        if HYPHEN_E_DOT in requirement_list:
+            requirement_list.remove(HYPHEN_E_DOT)
+        return requirement_list
+
+
+
+setup(
+    name = PROJECT_NAME,
+    version = VERSION,
+    author = AUTHOR,
+    description=DESCRIPTION,
+    packages= find_packages(),
+    install_requires = get_requirements_list()
+)
